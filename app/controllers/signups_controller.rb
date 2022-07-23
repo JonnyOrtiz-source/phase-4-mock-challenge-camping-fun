7454b92b55a84499038d74edbbb903c6b2b5490e
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-07-23 17:14:23 -0400

    Completed assignment

---
+++ app/controllers/signups_controller.rb
@@ -0,0 +1,16 @@
+class SignupsController < ApplicationController
+    def create
+        signup = Signup.create(signup_params)
+        if signup.valid?
+            render json: signup, status: :created
+        else
+            render json: {errors: ["validation errors"]}
+        end
+    end
+
+    private
+
+    def signup_params
+        params.permit(:time, :camper_id, :activity_id)
+    end
+end
